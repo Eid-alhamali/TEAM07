58c9b095cbc6e4d43abaa2cbb64ae097da5369e5
Beste Bayhan <127056457+bestebayhan04@users.noreply.github.com>
2024-11-19 21:02:29 +0300

    Refactored the backend structure (Modular MVC) (#14)
    - Introduces new controllers and configurations for the backend to align with the updated module structure.
    - Adds functionality for managing carts, orders, products, reviews, search, wishlist, and payments.
    - Implements key controllers: CartController, OrdersController, ProductsController, ReviewController, SearchController, WishlistController, and PaymentController.
    - Adds configuration setup for database connections via environment variables in appConfig.js.

---
+++ backend/src/config/app.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const config = {
+    root: path.join(__dirname, '/../../'),
+    controllers: path.join(__dirname, '/../controllers'),
+    sqlCon: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        charset: 'utf8mb4'
+    },
+    populateCon: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        charset: 'utf8mb4',
+        multipleStatements: true
+    }
+};
+
+module.exports = config;
